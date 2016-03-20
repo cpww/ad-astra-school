b02c841fe1a8db2b6eea33670d6bfa510a0518a5
Sean Dennison <sean.dennison@twintechs.com>
2016-03-20 12:56:22 -0400

    node-squarespace-server gulp tasks, skeleton of git gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 'use strict';
 
 var gulp          = require('gulp'),
+    shell 			  = require('gulp-shell'),
+    replace       = require('gulp-replace-task'),
+    envVars       = require('./.env'),
     sass          = require('gulp-sass'),
     sourcemaps    = require('gulp-sourcemaps'),
     autoprefixer  = require('gulp-autoprefixer');
@@ -29,3 +32,137 @@ gulp.task('watch', function () {
 });
 
 gulp.task('default', ['sass', 'watch']);
+
+gulp.task('localTemplateConf', function() {
+	gulp.src('template.conf')
+		.pipe(replace({
+			patterns: [
+				{
+					match: /SITEURL/g,
+					replacement: function () {
+						return envVars.url;
+					}
+				},
+				{
+					match: /PASSWORD/g,
+					replacement: function () {
+						return envVars.password;
+					}
+				}
+			]
+		}))
+		.pipe(gulp.dest("./"));
+});
+
+gulp.task('templateConfPushPrep', function() {
+gulp.src('template.conf')
+    .pipe(replace({
+			patterns: [
+				{
+					match: /http[^"]+/g,
+					replacement: function () {
+						return 'SITEURL';
+					}
+				},
+				{
+					match: /"password".*/g,
+					replacement: function () {
+						return '"password": "PASSWORD"';
+					}
+				}
+			]
+    }))
+		.pipe(gulp.dest("./"));
+});
+
+gulp.task('disablePushProd', function () {
+  return gulp.src('*.js', {read: false})
+    .pipe(shell([
+      'echo hello world'
+			// write git command to break push to origin
+      //'echo <%= f(file.path) %>',
+      //'ls -l <%= file.path %>'
+    ], {
+      templateData: {
+        f: function (s) {
+          return s.replace(/$/, '.bak')
+        }
+      }
+    }))
+})
+gulp.task('disablePushDev', function () {
+  return gulp.src('*.js', {read: false})
+    .pipe(shell([
+      'echo hello world'
+			// write git command to break push to origin
+      //'echo <%= f(file.path) %>',
+      //'ls -l <%= file.path %>'
+    ], {
+      templateData: {
+        f: function (s) {
+          return s.replace(/$/, '.bak')
+        }
+      }
+    }))
+})
+gulp.task('enablePushProd', function () {
+  return gulp.src('*.js', {read: false})
+    .pipe(shell([
+      'echo hello world'
+			// write git command to break push to origin
+      //'echo <%= f(file.path) %>',
+      //'ls -l <%= file.path %>'
+    ], {
+      templateData: {
+        f: function (s) {
+          return s.replace(/$/, '.bak')
+        }
+      }
+    }))
+})
+gulp.task('enablePushDev', function () {
+  return gulp.src('*.js', {read: false})
+    .pipe(shell([
+      'echo hello world'
+			// write git command to break push to origin
+      //'echo <%= f(file.path) %>',
+      //'ls -l <%= file.path %>'
+    ], {
+      templateData: {
+        f: function (s) {
+          return s.replace(/$/, '.bak')
+        }
+      }
+    }))
+})
+gulp.task('pushDev', function () {
+  return gulp.src('*.js', {read: false})
+    .pipe(shell([
+      'echo hello world'
+			// write git command to break push to origin
+      //'echo <%= f(file.path) %>',
+      //'ls -l <%= file.path %>'
+    ], {
+      templateData: {
+        f: function (s) {
+          return s.replace(/$/, '.bak')
+        }
+      }
+    }))
+})
+gulp.task('pushProd', function () {
+  return gulp.src('*.js', {read: false})
+    .pipe(shell([
+      'echo hello world'
+			// write git command to break push to origin
+      //'echo <%= f(file.path) %>',
+      //'ls -l <%= file.path %>'
+    ], {
+      templateData: {
+        f: function (s) {
+          return s.replace(/$/, '.bak')
+        }
+      }
+    }))
+})
+ 
